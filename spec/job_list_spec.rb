d021fe882e6d4aa7a1d0dfa375612b6f7b4555a8
Rob White <aca06rbw@gmail.com>
2017-02-08 15:32:08 +0000

    Moved on to the next scenario i.e. for a list with 1 job return the job. As before, wrote a failing test before writing the code to make it pass

--- spec/job_list_spec.rb
+++ spec/job_list_spec.rb
@@ -3,10 +3,14 @@ require 'job_list'
 
 RSpec.describe JobList, type: :model do
 
-  describe '.calculate_sequence' do
+  describe '.calculate_sequence_from_input' do
 
     it 'returns an empty collection if the input passed in is empty' do
       expect(described_class.calculate_sequence_from_input('')).to eq []
+    end
+
+    specify 'Given a basic job structure i.e. "a => " return a single job' do
+      expect(described_class.calculate_sequence_from_input('a => ')).to eq ['a']
     end
 
   end
