d139f5ef5e1a54f4c61946a0056a0f691315e0e5
Rob White <aca06rbw@gmail.com>
2017-02-10 23:06:06 +0000

    Refactored duplicated and verbose code into different methods

--- lib/job_sequence_builder.rb
+++ lib/job_sequence_builder.rb
@@ -17,15 +17,26 @@ class JobSequenceBuilder
 
   private
     def generate_sequence_for_job(job)
-      parent_job = self.job_hash.key(job)      
-      raise JobSequenceError.new('Jobs cannot have circular dependencies') if self.job_sequence.index(parent_job) && self.job_sequence.index(job) && self.job_sequence.index(parent_job) < self.job_sequence.index(job)
-      if self.job_sequence.include?(parent_job) && !self.job_sequence.include?(job) && !job.nil?
+      parent_job = self.job_hash.key(job)
+      raise JobSequenceError.new('Jobs cannot have circular dependencies') if circular_dependency?(parent_job, job)
+
+      if job_already_processed?(parent_job) && !job_already_processed?(job) && job
         self.job_sequence.insert(self.job_sequence.index(parent_job), job)
-      elsif !self.job_sequence.include?(job)
+      elsif !job_already_processed?(job)
         self.job_sequence << job
       end
 
       generate_sequence_for_job(parent_job) if parent_job
+    end
+
+    def job_already_processed?(job)
+      self.job_sequence.include?(job)
+    end
+
+    def circular_dependency?(parent_job, dependent_job)
+      parent_job_index = self.job_sequence.index(parent_job)
+      dependent_job_index = self.job_sequence.index(dependent_job)
+      parent_job_index && dependent_job_index && parent_job_index < dependent_job_index
     end
 
 end
