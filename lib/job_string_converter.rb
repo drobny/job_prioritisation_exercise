8c1cde1f9acc3cc91e8a645c8f78235872284dff
Rob White <aca06rbw@gmail.com>
2017-02-10 21:54:31 +0000

    Wrote the code to make the tests pass

--- lib/job_string_converter.rb
+++ lib/job_string_converter.rb
@@ -4,8 +4,11 @@ class JobStringConverter
   def initialize(job_string)
     @job_string = job_string
   end
-  
-  def convert_to_hash
 
+  def convert_to_hash
+    job_string.each_line.inject({}) do |collection, line|
+      job, dependent_job = line.gsub(/\s+/, '').split('=>')
+      collection.merge job => dependent_job
+    end
   end
 end
