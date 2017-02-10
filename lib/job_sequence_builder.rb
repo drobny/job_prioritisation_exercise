ff5a7d5bbd5e17f7454c0eac401ae37c45ea9a28
Rob White <aca06rbw@gmail.com>
2017-02-10 14:20:47 +0000

    Extracted some duplicated code into a single method when seeing if the job is already in the job sequence

--- lib/job_sequence_builder.rb
+++ lib/job_sequence_builder.rb
@@ -9,7 +9,7 @@ class JobSequenceBuilder
     job_string.each_line do |line|
       job, dependent_job = line.gsub(/\s+/, '').split('=>')
       if dependent_job.nil?
-        self.job_sequence << job unless self.job_sequence.include?(job)
+        self.job_sequence << job if job_not_in_current_sequence?(job)
       else
         add_to_sequence(job, dependent_job)
       end
@@ -21,13 +21,17 @@ class JobSequenceBuilder
       job_index   = self.job_sequence.index(job)
       dependent_job_index = self.job_sequence.index(dependent_job)
 
-      if job_index && !self.job_sequence.include?(dependent_job)
+      if job_index && job_not_in_current_sequence?(dependent_job)
         self.job_sequence.insert(job_index, dependent_job)
-      elsif dependent_job_index && !self.job_sequence.include?(job)
+      elsif dependent_job_index && job_not_in_current_sequence?(job)
         self.job_sequence.insert(dependent_job_index + 1, job)
-      elsif !self.job_sequence.include?(job) && !self.job_sequence.include?(dependent_job)
+      elsif job_not_in_current_sequence?(job) && job_not_in_current_sequence?(dependent_job)
         self.job_sequence = [dependent_job, job] + self.job_sequence
       end
+    end
+
+    def job_not_in_current_sequence?(job)
+      !self.job_sequence.include?(job)
     end
 
 end
